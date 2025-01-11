c0c1578c6c07fe977d4198f94e88bbd1670cf188
a-s-adam <55128811+a-s-adam@users.noreply.github.com>
2025-01-11 08:44:33 -0800

    Setup initial project structure with React, Chakra UI, and Express

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, theme } from '@chakra-ui/react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import FindTeam from './pages/FindTeam';
 
+// Use the default theme for now
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Router>
         <Navbar />
         <Routes>
